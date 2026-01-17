5d48388b4507c88c4eb1ea5d0633062e1af835b1
Darren-Putra <darren.misyantono05@gmail.com>
2026-01-17 18:40:43 +0800

    feat: update webhook deploy route to exclude additional middleware

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Livewire\Pages;
 use App\Livewire\Admin;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
-
-Route::post('/_deploy/testing', function () {
-    return (require base_path('deploy/webhook-testing.php'));
-})->withoutMiddleware([
-    VerifyCsrfToken::class,
-]);
 
 Route::get('resto' , function () {
     return view('resto_app');
@@ -47,3 +40,15 @@ Route::middleware(['auth', 'admin'])
     });
 
 require __DIR__.'/settings.php';
+
+// ================= WEBHOOK DEPLOY (NO MIDDLEWARE) =================
+Route::post('/_deploy/testing', function () {
+    return require base_path('deploy/webhook-testing.php');
+})->withoutMiddleware([
+    \Illuminate\Session\Middleware\StartSession::class,
+    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    \Illuminate\Cookie\Middleware\EncryptCookies::class,
+    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+    \App\Http\Middleware\SetLocale::class,
+]);
+// =================================================================
