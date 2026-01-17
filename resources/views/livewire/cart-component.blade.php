4ebc653dace8720f5adf6659c5e547ddc269a1ab
Darren-Putra <darren.misyantono05@gmail.com>
2026-01-18 01:43:33 +0800

    fix

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -60,7 +60,7 @@
 
                         <div
                             class="w-20 h-20 bg-stone-100 rounded-xl overflow-hidden flex-shrink-0 border border-stone-50">
-                           <img src="{{ $item['imageUrl'] }}" alt="{{ $item['name'] }}"
+                           <img src="{{ $item['image'] }}" alt="{{ $item['name'] }}"
                                 class="w-full h-full object-cover">
                         </div>
 
