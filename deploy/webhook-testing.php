335aeea80ba34cc02b1e6ba4cff161bd63f23b8d
Darren-Putra <darren.misyantono05@gmail.com>
2026-01-17 19:20:41 +0800

    testing

--- deploy/webhook-testing.php
+++ deploy/webhook-testing.php
@@ -1,3 +1,6 @@
 <?php
 
-return response('DEPLOY SCRIPT HIT', 200);
+$log = base_path('storage/logs/webhook-debug.log');
+file_put_contents($log, now().' HIT'.PHP_EOL, FILE_APPEND);
+
+return response('LOG OK', 200);
